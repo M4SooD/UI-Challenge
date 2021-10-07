2c3c90af163ad53fbea499684f7f4493acb0844f
Alireza Razinejad <a.razinejad@gmail.com>
2021-10-07 19:56:10 +0330

    feat: Initialized for creating buttons randomly

--- src/script.js
+++ src/script.js
@@ -2,19 +2,35 @@ const numbers = document.getElementsByClassName('password--numbers');
 const inputs = document.getElementsByClassName('password--field');
 const clearBtn = document.getElementById('clearBtn');
 
-// Document Functionality
+const randomNumbers = []; // from 0 to 9
+
+// region Document Functionality
 
 document.body.addEventListener('keydown', (event) => {
     if (event.key === 'Backspace') {
         clearLastValuedInput();
-    } else if ((event.key >= 0 && event.key <= 9)) { 
+    } else if ((event.key >= 0 && event.key <= 9)) {
      setInputValue(event.key)
     }
 
     if (event.key >= 0 && event.key <= 9) {
         setInputValue(event.key);
     }
-})
+});
+
+const createButtons = () => {
+    const container = document.getElementById('buttons-container');
+
+    const button = document.createElement('button');
+    button.classList.add('btn');
+    button.innerText = '5';
+}
+
+const createRandomNumbers = () => {
+    // while () {} do
+}
+
+// endregion
 
 
 // region Button Functionality
